53d44940ac990fbad23fb0b77a19a946e095d5a7
Seiwan <sewan.khalni@gmail.com>
2023-07-19 00:32:48 +0330

    update shipping filter

--- src/context/filter_context.tsx
+++ src/context/filter_context.tsx
@@ -117,7 +117,7 @@ export const FilterProvider: React.FC<ContextProps> = ({ children }) => {
         value = Number(value);
       }
       if (name === 'shipping') {
-        value = Boolean(value);
+        value = event.target.checked;
       }
     }
 
